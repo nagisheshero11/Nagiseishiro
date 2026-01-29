8911f4929ee21ab629f1fd85c77fee4a4073cec1
Hemanth Sai <nagiseishiro@Hemanths-MacBook-Air-2.local>
2026-01-30 00:15:04 +0530

    added background and done few styling and seperated the intro styling to intro.css from index.css

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -1,17 +1,34 @@
+import "./style/Intro.css";
+
 function Intro() {
   return (
     <section className="intro">
+      {/* background icons */}
+      <div className="intro-bg">
+        <img src="/bg_icon1.jpg" className="bg-icon icon-1" alt="" />
+        <img src="/bg_icon2.jpg" className="bg-icon icon-2" alt="" />
+        <img src="/bg_icon3.jpg" className="bg-icon icon-3" alt="" />
+        <img src="/bg_icon4.jpg" className="bg-icon icon-4" alt="" />
+      </div>
+
+      {/* foreground content */}
+      <div className="intro-container">
         <section className="intro-text">
-            <h1>Hey, This is Nagi Seishiro 👋</h1>
-            <p>Feel free to explore me. this is who i am, this is how i live</p>
+          <h1>Hey, I'm Nagi Seishiro 👋</h1>
+          <p>
+            I am just a mediocre guy who loves to do things I like and be happy
+            with my life and wanna be a software engineer someday and want to belong in the life i wanted to live.
+          </p>
         </section>
 
         <section className="intro-image">
-            <img src="/profile.png" alt="NagiSeishiro"/>
+          <img src="/profile.png" alt="NagiSeishiro" />
         </section>
+      </div>
     </section>
-
   );
 }
 
 export default Intro;
+
+
