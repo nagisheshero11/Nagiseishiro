a019cae9336d1a6ccef3e5303675c4831b9c06a7
Hemanth Sai <nagiseishiro@Hemanths-MacBook-Air-2.local>
2026-01-29 13:31:51 +0530

    added profile picture and styled it in index.css

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -1,11 +1,17 @@
-
 function Intro() {
-    return(
-        <>
-            <h1 className="intro">Hey, This is Nagi Seishiro 👋</h1>
-            <p>Feel free to explore me this is who i am this is how i live</p>
-        </>
-    )
+  return (
+    <section className="intro">
+        <section className="intro-text">
+            <h1>Hey, This is Nagi Seishiro 👋</h1>
+            <p>Feel free to explore me. this is who i am, this is how i live</p>
+        </section>
+
+        <section className="intro-image">
+            <img src="/profile.png" alt="NagiSeishiro"/>
+        </section>
+    </section>
+
+  );
 }
 
 export default Intro;
